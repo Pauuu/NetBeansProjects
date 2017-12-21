e98f3391814ead9ee9c07e5812e0560fe51762a5
Pau <belcegorx@gmail.com>
2017-12-21 23:53:25 +0100

    Solo guarda un numero par
    Solo guarda un numero par inicial

--- introduccionObjetos/src/arrayList/ArrayList.java
+++ introduccionObjetos/src/arrayList/ArrayList.java
@@ -15,16 +15,46 @@ public class ArrayList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        calculoPrimos();
     }
 
     private static void calculoPrimos() {
         int numero;
         int posicion;
         int residuo;
-        int primos[] = new int [100];   //almacenamietno de numeros primos
+        int primos[] = new int[100];   //almacenamietno de numeros primos
         boolean primo;
-        
 
+        numero = 4;
+
+        for (posicion = 0; posicion < 100;) {       //bucle hasta llegar a 100 primos   
+            for (;; numero++) {           //bucle que busca numeros primos
+                if (numero == 2) {
+                    primo = true;
+                    break;
+                } else if (numero % 2 == 0) {
+                    primo = false;
+                    break;
+                } else {
+                    for (int primoNum = 0; primoNum <= posicion; primoNum++) {
+                        residuo = numero % primos[primoNum];
+                        if (residuo == 0) {
+                            primo = false;
+                            break;
+                        } else {
+                            primo = true;
+                        }
+                    }
+                }
+            }
+            if (primo = true) {
+                primos[posicion] = numero;
+                posicion++;
+            }
+        }
+
+        for (posicion = 0; posicion < 100; posicion++) {
+            System.out.println(primos[posicion]);
+        }
     }
 }
