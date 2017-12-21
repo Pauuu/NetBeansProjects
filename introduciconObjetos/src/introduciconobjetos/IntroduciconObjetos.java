13dac8ba40884318d8d378e38d45152d4fee45f2
Pau <belcegorx@gmail.com>
2017-12-21 23:06:17 +0100

    si

--- introduciconObjetos/src/introduciconobjetos/IntroduciconObjetos.java
+++ introduciconObjetos/src/introduciconobjetos/IntroduciconObjetos.java
@@ -5,16 +5,23 @@ import java.util.ArrayList;
 public class IntroduciconObjetos {
 
     public static void main(String[] args) {
-        ArrayList<Integer> myList = new ArrayList<Integer>(5);
+        ArrayList<Integer> myList = new ArrayList<>(5);
         myList.add(1);
         myList.add(5);
         myList.add(3);
         myList.add(25);
         myList.add(93);
-        
-        for (Integer x : myList){
+
+        myList.forEach((x) -> {
             System.out.println(x);
-        }
+        });
+        System.out.println("-" + myList.size());
+
+        myList.trimToSize();
+        myList.forEach((x) -> {
+            System.out.println(x);
+        });
+
+        System.out.println(myList.size());
     }
-    
 }
